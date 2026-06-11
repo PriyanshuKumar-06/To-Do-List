266a8200e77d29c0d79c1b14aed79b952eb153f7
unknown <priyanshu6.uk@gmail.com>
2026-06-11 20:13:57 +0530

    Remove Button added

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 let textBox = document.querySelector(".add");
 let button = document.getElementById("addbutt");
 let container = document.querySelector(".container");
-let typebox = document.querySelector(".add")
+let typebox = document.querySelector(".add");
+let remove = document.querySelector(".remove");
 let dataToAdd;
 function addtask() {
   dataToAdd = textBox.value;
   if (!dataToAdd.trim()) return;
   let task = document.createElement("div");
   task.className = "task";
-  task.innerHTML = `<input type="checkbox" class="checkbox"><p class="info">${dataToAdd}</p>`;
+  task.innerHTML = `<input type="checkbox" class="checkbox"><p class="info">${dataToAdd}</p><span class = "remove"><img src="assets/icons8-delete-30.png" alt="bin"></span>`;
   container.append(task);
   textBox.value = "";
   return dataToAdd;
 }
-typebox.addEventListener("keydown", (event)=>{
-  if(event.key == 'Enter'){
-    addtask()
+typebox.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    addtask();
   }
-})
+});
 button.addEventListener("click", () => {
   addtask();
 });
 
-button.addEventListener("keydown", (event)=>{
-  if(event.key == 'Enter'){
-    addtask()
+button.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    addtask();
   }
-})
+});
 container.addEventListener("change", (e) => {
   if (e.target.classList.contains("checkbox")) {
     let task = e.target.parentElement;
@@ -38,8 +39,14 @@ container.addEventListener("change", (e) => {
       task.style.color = "white";
     } else {
       info.style.textDecoration = "none";
-      task.style.backgroundColor = "#d0c9b8"
+      task.style.backgroundColor = "#d0c9b8";
       task.style.color = "black";
     }
+  }
+});
+container.addEventListener("click", (e) => {
+  if (e.target.closest(".remove")){
+    let bin = e.target.parentElement.parentElement
+    bin.remove()
   }
 });
